c84a2abbe642d6c3380b56d5e74544da3bb6313f
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-20 21:03:36 +0000

    created and tested player class

---
+++ app/tests/player_test.py
@@ -0,0 +1,10 @@
+import unittest
+from app.models.player import Player
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Player('Chris', 'rock')
+        
+        
+    def test_player_has_name(self):
+        self.assertEqual('Chris', self.player1.name)
